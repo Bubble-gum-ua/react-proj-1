dacbe343efca7e2234d667f939268814e4b0439b
Yurii <bubble.gumgum1992@gmail.com>
2020-05-09 17:56:32 +0300

    Added routings for menubar

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Dialogs/>
+                <Route path="/dialogs" component={Dialogs}/>
+                <Route path="/profile" component={Profile}/>
             </div>
-
-            {/*<Profile/>*/}
         </div>
+        </BrowserRouter>
     );
 };
 
