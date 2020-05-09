6cb91c74d6a38f009c4c4ba924e72f2a9bafe4cc
Bubble_gum_ua <bubblegum.gum1992@gmail.com>
2020-05-10 02:21:42 +0300

    Styles for sidebar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from "./Header.module.css";
 
 const Header = () => {
     return <header className={s.header}>
-        <img src="https://png.pngtree.com/element_pic/16/11/03/dda587d35b48fd01947cf38931323161.jpg"/>;
+        <img src="https://png.pngtree.com/element_pic/16/11/03/dda587d35b48fd01947cf38931323161.jpg"/>
     </header>
 };
 
