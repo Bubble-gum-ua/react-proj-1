44ad95129091c4f5bc386229c411234808fa5bbc
Bubble_gum_ua <bubblegum.gum1992@gmail.com>
2020-05-10 03:55:24 +0300

    Styles part 3

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -9,6 +9,7 @@ const ProfileInfo = () => {
             <div>
                 <img className={s.headimg} src="https://cdn.beach-inspector.com/static/awards/lp-header.jpg?w=1200&h=400&fit=crop"/>
             </div>
+            <div className={s.cardProf}>
             <div>
                 <img className={s.ava}
                      src="https://vignette.wikia.nocookie.net/wow/images/e/e5/200px-Bolvar%2C_Fireblood.jpg/revision/latest?cb=20191029151859&path-prefix=ru"/>
@@ -16,6 +17,7 @@ const ProfileInfo = () => {
             </div>
             <div className={s.discriptionBlock}>
                 ava+disc
+            </div>
             </div>
 
         </div>
