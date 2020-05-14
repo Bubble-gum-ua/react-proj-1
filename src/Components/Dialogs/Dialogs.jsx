aa18af41d594a001ea2bf8d0afe6083bace7c228
Yurii <bubble.gumgum1992@gmail.com>
2020-05-14 17:28:02 +0300

    Added Redux folder. Migrate data from index.js to state.js

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -7,8 +7,8 @@ import MessageItem from "./Message/Message";
 const Dialogs = (props) => {
 
 
-    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
-    let messagesElements = props.messages.map(m => <MessageItem message={m.message}/>)
+    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = props.state.messages.map(m => <MessageItem message={m.message}/>)
 
     return (
         <div className={s.dialogs}>
--- src/index.js
+++ src/index.js
@@ -3,37 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import state from "./Redux/state";
 
 
-let posts = [
-    {
-        id: 1, message: "Hi, how are you?", likesCount: 15, photoAvatar: <img
-            src="https://i.pinimg.com/originals/3d/6f/b2/3d6fb2056e94691bf76a4426dd826aa4.jpg"/>
-    },
-    {
-        id: 2, message: "It's my first post", likesCount: 20, photoAvatar: <img
-            src="https://cdna.artstation.com/p/assets/images/images/019/650/704/large/ynorka-chiu-jaina-proudmoore-by-ynorka.jpg?1564434256"/>
-    },]
 
-let dialogs = [
-    {id: 1, name: 'Ozzeron'},
-    {id: 2, name: 'Lusyi'},
-    {id: 3, name: 'Lena'},
-    {id: 4, name: 'Freddy'},
-    {id: 5, name: 'Busya'}
-]
-
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How is your mood?'},
-    {id: 3, message: 'Yo'},
-    {id: 4, message: 'Yo'},
-    {id: 5, message: 'Yo'}
-]
 
 ReactDOM.render(
     <React.StrictMode>
-        <App posts={posts} dialogs={dialogs} messages={messages}/>
+        <App state={state}/>
     </React.StrictMode>,
     document.getElementById('root')
 );
