8cde706372b56f69a5d4f66e3d82d55ddfcd2462
Bubble_gum_ua <bubblegum.gum1992@gmail.com>
2020-07-23 15:02:26 +0300

    Added "axios" library. 51 lesson

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,34 +1,16 @@
 import React from "react";
 import s from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../.././Assets/Images/User.png"
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-            id: 1,
-            photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBAq7blqdI8FbCfEvsyPkQmfy3XC4gYhulmg&usqp=CAU",
-            followed: false,
-            fullName: "Barry",
-            status: "Love big tit's",
-            location: {city: "Minesota", country: "USA"}
-        },
-            {
-                id: 2,
-                photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBAq7blqdI8FbCfEvsyPkQmfy3XC4gYhulmg&usqp=CAU",
-                followed: true,
-                fullName: "Ammy",
-                status: "I'm wait for DickHead",
-                location: {city: "Calgary", country: "Canada"}
-            },
-            {
-                id: 3,
-                photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBAq7blqdI8FbCfEvsyPkQmfy3XC4gYhulmg&usqp=CAU",
-                followed: false,
-                fullName: "DickHead",
-                status: "Stupid boy",
-                location: {city: "Kharkiv", country: "Ukraine"}
-            },])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items)
+            });
+
     }
 
     return <div>
@@ -36,7 +18,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                <span>
                    <div>
-                       <img src={u.photoURL} className={s.usersPhoto}/>
+                       <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.usersPhoto}/>
                    </div>
                    <div>
                        {u.followed
@@ -51,12 +33,12 @@ let Users = (props) => {
                </span>
                 <span>
                    <span>
-                       <div>{u.fullName}</div>
+                       <div>{u.name}</div>
                        <div> {u.status}</div>
                    </span>
                    <span>
-                       <div>{u.location.country}</div>
-                       <div>{u.location.city}</div>
+                       <div>{"u.location.country"}</div>
+                       <div>{"u.location.city"}</div>
                </span>
                </span>
             </div>)
