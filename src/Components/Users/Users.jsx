753d1680f625deeaf62f39116418305b9ccf7ecf
Yurii <bubble.gumgum1992@gmail.com>
2020-07-26 16:29:03 +0300

    Added componentDidMount. Lifetime components. Lesson 54

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,15 +5,12 @@ import userPhoto from "../.././Assets/Images/User.png"
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items)
             });
-
     }
-
 
     render() {
         return <div>
