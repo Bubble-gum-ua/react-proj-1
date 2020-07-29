6a136790f15fa00144e64b93bb3711cc3f4756d8
Bubble_gum_ua <bubblegum.gum1992@gmail.com>
2020-07-29 16:49:48 +0300

    Added loginBar. Lesson 61

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
 import {Route} from "react-router-dom";
@@ -10,7 +9,7 @@ import Settings from "./Components/Settings/Settings";
 import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import UsersContainer from "./Components/Users/UsersContainer";
 import FriendsFolderContainer from "./Components/Friendfolder/FriendfolderContainer";
-
+import HeaderContainer from "./Components/Header/HeaderContainer";
 
 
 const App = (props) => {
@@ -18,17 +17,17 @@ const App = (props) => {
     return (
 
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Route path="/dialogs" render={() => <DialogsContainer />}/>
-                <Route path="/profile/:userId?" render={() => <ProfileContainer  />}/>
+                <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
                 <Route path="/news" render={() => <News/>}/>
                 <Route path="/music" render={() => <Music/>}/>
                 <Route path="/settings" render={() => <Settings/>}/>
                 <Route path="/users" render={() => <UsersContainer/>}/>
             </div>
-            <FriendsFolderContainer />
+            <FriendsFolderContainer/>
         </div>
 
     );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import s from "./Header.module.css";
 import Logo from "../../Assets/Images/Logo.png"
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src={Logo}/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={"/login"}> Login </NavLink>}
+        </div>
     </header>
 };
 
