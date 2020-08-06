6494f381ca837e3a8b9003992393d218273ade42
Bubble_gum_ua <bubblegum.gum1992@gmail.com>
2020-08-06 16:12:09 +0300

    Dal. Lesson 63

---
+++ src/API/Api.js
@@ -0,0 +1,23 @@
+import * as axios from "axios";
+
+
+const instatnce = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "6294e4f4-48cf-4973-aca5-f3512edd3216"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+
+        return instatnce.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+
+    }
+};
+
+
