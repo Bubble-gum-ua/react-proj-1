26441ccbc5a0806e058e7f885954dd8ddce0ae3f
Bubble_gum <bubble.gumgum1992@gmail.com>
2020-12-01 23:29:52 +0200

    Styles to login form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import {Field, reduxForm} from "redux-form";
 import {Input} from "../Common/FormsControls/FromsControls";
 import {maxLengthCreator, required} from "../../Utils/Validators/validators";
+import styles from"./Login.module.css"
 
 const maxLength10 = maxLengthCreator(10);
 const LoginForm = (props) => {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit} className={styles.loginForm}>
             <div>
                 <Field placeholder={"Login"} name={"login"} validate={[required,maxLength10]} component={Input}/>
             </div>
             <div>
                 <Field placeholder={"Password"} name={"password"} validate={[required,maxLength10]} component={Input}/>
             </div>
-            <div>
+            <div className={styles.checkbox}>
                 <Field type={"checkbox"} name={"rememberMe"} component={Input} validate={[required]}/> remember me, bitch
             </div>
             <div>
@@ -34,7 +35,7 @@ const Login = () => {
     const onSubmit = (formData) => {
         console.log(formData);
     }
-    return <div>
+    return <div className={styles.loginForm}>
         <h1>LOgIN BRO</h1>
         <LoginReduxForm onSubmit={onSubmit}/>
     </div>
