512e9d82efcae4afc7eb903dcac3fb79b1653acf
Bubble_gum <bubble.gumgum1992@gmail.com>
2020-12-04 22:35:46 +0200

    Refactor form styles

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,25 +2,30 @@ import React from "react";
 import {Field, reduxForm} from "redux-form";
 import {Input} from "../Common/FormsControls/FromsControls";
 import {maxLengthCreator, required} from "../../Utils/Validators/validators";
-import styles from"./Login.module.css"
+import styles from "./Login.module.css"
 
 const maxLength10 = maxLengthCreator(10);
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit} className={styles.loginForm}>
             <div>
-                <Field placeholder={"Login"} name={"login"} validate={[required,maxLength10]} component={Input} type={"text"}/>
-            </div>
-            <div>
-                <Field placeholder={"Password"} name={"password"} validate={[required,maxLength10]} component={Input} type={"text"}/>
-            </div>
-            <div className={styles.checkbox}>
-                <Field type={"checkbox"} name={"rememberMe"} component={Input} validate={[required]}/> remember me, bitch
-            </div>
-            <div>
-                <button>
-                    Login
-                </button>
+                <div>
+                    <Field placeholder={"Login"} name={"login"} validate={[required, maxLength10]} component={Input}
+                           type={"text"}/>
+                </div>
+                <div>
+                    <Field placeholder={"Password"} name={"password"} validate={[required, maxLength10]}
+                           component={Input} type={"text"}/>
+                </div>
+                <div>
+                    <Field type={"checkbox"} name={"rememberMe"} component={Input} validate={[required]}/> remember me,
+                    bitch
+                </div>
+                <div>
+                    <button>
+                        Login
+                    </button>
+                </div>
             </div>
         </form>
     )
@@ -35,7 +40,7 @@ const Login = () => {
     const onSubmit = (formData) => {
         console.log(formData);
     }
-    return <div className={styles.loginForm}>
+    return <div className={styles.mainFormwrapp}>
         <h1>LOgIN BRO</h1>
         <LoginReduxForm onSubmit={onSubmit}/>
     </div>
